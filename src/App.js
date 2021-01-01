7def9684010d40708f4fec94f4765615d20c5513
Ritesh Bucha <bucharitesh@gmail.com>
2021-01-02 01:21:48 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Loader from './Components/Loader/Loader';
+
 function App() {
   return (
     <div className="App">
-      
+      {/* Header */}
+      {/* Body */}
+      {/* Footer */}
+      <Loader/>
     </div>
   );
 }
