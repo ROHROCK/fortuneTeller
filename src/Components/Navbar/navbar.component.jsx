8c570ce942a8ee9f6369bcdd5e1a25ca72525928
kausarsayyed20 <kausarsayyed.20@gmail.com>
2021-01-02 20:39:34 +0530

    Added Navbar component

--- src/Components/Navbar/navbar.component.jsx
+++ src/Components/Navbar/navbar.component.jsx
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
-import {Navbar,Nav, NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+//import "./navbar.css";
+import { Link, BrowserRouter } from "react-router-dom";
 class NavbarComponent extends Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>   
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
-        )
+            <nav className="navbar navbar-light bg-light" href="#" >
+                <a className="navbar-brand">
+                    <BrowserRouter>
+                        <Link to="/">
+                            <img
+                                src="/Assets/Icons/logo.png"
+                                width="35"
+                                height="35"
+                                className="d-inline-block align-top"
+                                alt=""
+                            />
+                        </Link>
+                    </BrowserRouter>
+                    <div className="text-align-center d-inline-block pl-2">Fortune Teller</div>
+                </a>
+                <div className="navbar_container">
+                    <Navbar bg="light" expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto" >
+                                <Nav.Link href="#home"style={{color:"blue"}}>HOME</Nav.Link>{" "}
+                                <Nav.Link href="#about" style={{color:"blue"}}>ABOUT</Nav.Link>{" "}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </nav>
+        );
     }
 }
 export default NavbarComponent;
