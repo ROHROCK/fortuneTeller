5b2e4bf50282d1a32bbe9a12193c5c1309e01313
Rohit Agharkar <rohitsa40@gmail.com>
2021-01-02 12:05:39 +0530

    Added flexbox to home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Loading from "./Components/Loader/Loader";
 const Home = lazy(() => import("./Pages/Home"));
 const About = lazy(() => import("./Pages/About"));
 const NotFound = lazy(() => import("./Pages/About"));
+const Fortune = lazy(() =>
+  import("./Components/Fortune-Teller/fortune-teller.component")
+);
 
 function App() {
   return (
@@ -23,6 +26,7 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/fortune" component={Fortune} />
         <Route component={NotFound} />
       </Switch>
     </Suspense>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,133 @@
 import React from "react";
+import Table from "../Assets/Icons/table.svg";
+import Hand from "../Assets/Icons/hand.svg";
+import Flower from "../Assets/Icons/flower.svg";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div
       style={{
-        height: "100vh",
         display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
+        margin: "20px",
       }}
     >
-      <button>Click Here to see your fortune !</button>
+      <div
+        id="child1"
+        style={{
+          display: "flex",
+          height: "90vh",
+          flexGrow: "1",
+        }}
+      >
+        <div
+          id="box-div"
+          style={{
+            display: "flex",
+            flex: "1 1 0px",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <div
+            style={{
+              alignSelf: "center",
+            }}
+          >
+            <img
+              src={Hand}
+              style={{
+                width: "10vh",
+                align: "flex-end",
+                height: "30vh",
+              }}
+              alt="Flower"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="child2"
+        style={{
+          display: "flex",
+          height: "90vh",
+          flexGrow: "1",
+        }}
+      >
+        <div
+          id="svg-div"
+          style={{
+            display: "flex",
+            height: "90vh",
+            flex: "1 1 0px",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            id="button-div"
+            style={{
+              alignSelf: "center",
+              justifySelf: "center",
+            }}
+          >
+            <button src={Table}>Click Here to check fortune</button>
+          </div>
+
+          <div
+            id="table-div"
+            style={{
+              alignSelf: "center",
+              justifySelf: "flex-end",
+            }}
+          >
+            <img
+              src={Table}
+              style={{
+                width: "40vh",
+                alignS: "flex-end",
+                height: "30vh",
+              }}
+              alt="Table"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="child3"
+        style={{
+          display: "flex",
+          height: "90vh",
+          flexGrow: "1",
+        }}
+      >
+        <div
+          id="box-div"
+          style={{
+            display: "flex",
+            flex: "1 1 0px",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              alignSelf: "center",
+            }}
+          >
+            <img
+              src={Hand}
+              style={{
+                width: "10vh",
+                align: "flex-end",
+                height: "30vh",
+              }}
+              alt="Flower"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
