fa77696c812c250042e2d281c869722d2018bd45
kausarsayyed20 <kausarsayyed.20@gmail.com>
2021-01-02 13:33:42 +0530

    Box component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Color_Box from './Components/Boxes/box.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Color_Box/>
     </div>
   );
 }
