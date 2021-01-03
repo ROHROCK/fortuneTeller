b956c6db3adb7dfad15fc4af3ef9b300954da74d
Ritesh Bucha <bucharitesh@gmail.com>
2021-01-03 17:32:02 +0530

    about us

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,11 @@ import "./About.css";
 function About() {
   return (
     <div class="center">
+      <h1 className="About_header">Tech Stack Used</h1>
+      <div className="">
+        
+      </div>
+      <h1 className="About_header">Our Team</h1>
       <div class="card">
         <div class="additional">
           <div class="user-card">
