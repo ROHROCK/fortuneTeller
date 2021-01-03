6e0eeef4ea1d6f7f856910f15d5bb3e6a70ceeb4
Rohit Agharkar <rohitsa40@gmail.com>
2021-01-03 19:28:27 +0530

    Added HTML5 icon

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -11,6 +11,7 @@ function About() {
          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/540px-Npm-logo.svg.png" alt="Npm"/>
          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="React"/>
          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png" alt="Css"/>
+         <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="Html5"/>
       </div>
       <h1 className="About_header">Our Team</h1>
       <Team />
