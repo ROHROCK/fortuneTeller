078af5d2d595b263e2d78b6508528634a9fbc207
Rohit Agharkar <rohitsa40@gmail.com>
2021-01-03 13:12:05 +0530

    Added alt for images about us

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import ReactLogo from '../Assets/Icons/techicons/react.svg';
+import FirebaseLogo from '../Assets/Icons/techicons/firebase.svg';
+import NpmLogo from '../Assets/Icons/techicons/npm.svg';
+import CssLogo from '../Assets/Icons/techicons/css.svg';
+import HtmlLogo from '../Assets/Icons/techicons/html.svg';
 
 function About() {
     return (
@@ -6,10 +11,16 @@ function About() {
             <div style={{backgroundColor:"green",height:"20vh",textAlign:"center"}}>
                 <h1>Welcome to Fortune Teller "Add globe emoji here"!</h1>
                 <h2>This is project is inspired by the New year new hack conducted by MLH </h2>
+                {/* eslint-disable-next-line */}
                 <a href="#"><p>Click Here to learn more</p></a>
             </div>
             <div id="techStack">
                 <h1>Tech stack used</h1>
+                <img src={ReactLogo} style={{width:'10vh',height:'10vh'}} alt='reactlogo' />
+                <img src={FirebaseLogo} style={{width:'10vh',height:'10vh'}} alt='firebaselogo'/>
+                <img src={NpmLogo} style={{width:'10vh',height:'10vh'}} alt='npmlogo'/>
+                <img src={CssLogo} style={{width:'10vh',height:'10vh'}} alt='csslogo'/>
+                <img src={HtmlLogo} style={{width:'10vh',height:'10vh'}} alt='htmllogo'/>
             </div>
             <div id="creator">
                 <h1>Meet the Creators !</h1>
