38018ed36248fb84cc84838639ccdb3301111fe0
Ritesh Bucha <bucharitesh@gmail.com>
2021-01-03 15:16:54 +0530

    Update creator.jsx

--- src/Components/Creator/creator.jsx
+++ src/Components/Creator/creator.jsx
@@ -5,10 +5,12 @@ class creator extends Component {
     return (
       <div>
           <div class="card" style="width: 18rem;">
+            {/* eslint-disable-next-line */}
             <img src="..." class="card-img-top" alt="profilepicture" />
             <div class="card-body">
                 <h5 class="card-title">Rohit</h5>
                 <p class="card-text">Hey I am rohit , Nice to meet you !</p>
+                {/* eslint-disable-next-line */}
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
            </div>
